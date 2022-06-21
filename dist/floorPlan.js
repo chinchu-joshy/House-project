ae4bf567ac735e608f52ad93e3333ce761600a35
Chinchu Joshy <chinchu.joshy@ftsindia.in>
2022-06-21 14:36:40 +0530

    changed path6

--- dist/floorPlan.js
+++ dist/floorPlan.js
@@ -1,46 +1,10 @@
 
 import { ARButton } from './js/arbutton.js';
 let camera,
-
   scene,
   renderer,
   light,
   clock,
-  cameraControls,
-  textureBase,
-  textureWall,
-  texture,
-  textureRoof,
-  ventTexture,
-  found,
-  startpoint,
-  mouse3D,
-  intersects,
-  xPosition,
-  yPosition,
-  composer,
-  outlinePass,
-  selectedObjects,
-  bbox,
-  bbox2,
-  mousemove,
-  mainpoint,
-  endpoint,
-  endpointLeft,
-  startpointLeft,
-  endpointBottomRight,
-  startpointBottomRight,
-  endpointBottomLeft,
-  startpointBottomLeft,
-  intersectPoint,
-  obj,
-  saltbox,
-  vent,
-  door,
-  collision = false,
-  frontChildren,
-  controller,
-  backChildren;
 /* -------------------------------- constants ------------------------------- */
 const canvas = document.querySelector("canvas.webgl");
 /* ----------------------------- clipping plane ----------------------------- */
@@ -74,56 +38,7 @@ function init() {
   renderer.localClippingEnabled = true;
   renderer.xr.enabled=true
  
-  // document.body.appendChild(renderer.domElement);
-
-//   var dirLight = new THREE.DirectionalLight(0xffffff, 0.4);
-//   dirLight.position.set(90, 30, 0);
-//   dirLight.castShadow = true;
-//   dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
-
-//   var ambient = new THREE.AmbientLight(0xffffff, 0.8);
-//   scene.add(ambient);
-//   var dirLightright = new THREE.DirectionalLight(0xffffff, 0.5);
-//   dirLightright.position.set(8, 4, -12);
-//   dirLightright.castShadow = true;
-//   dirLightright.shadow.mapSize = new THREE.Vector2(1024, 1024);
-//   scene.add(dirLightright);
-//   light = new THREE.PointLight(0xffffff, 0.9, 100);
-//   light.position.set(-10, 10, 92);
-//   const light2 = new THREE.PointLight(0xffffff, 0.9, 100);
-//   light2.position.set(20, 10, 92);
-//   scene.add(light2);
-//   scene.add(light);
-//   const sphereSize = 1;
-//   const pointLightHelper = new THREE.PointLightHelper(
-//     light,
-//     sphereSize,
-//     0xfff000
-//   );
-//   scene.add(pointLightHelper);
-//   const pointLightHelper2 = new THREE.PointLightHelper(
-//     light2,
-//     sphereSize,
-//     0xfff000
-//   );
-//   scene.add(pointLightHelper2);
-  
-  /* ---------------------------- post proccessing ---------------------------- */
-//   composer = new EffectComposer(renderer);
-//   const renderPass = new RenderPass(scene, camera);
-//   composer.addPass(renderPass);
-//   const glitchPass = new GlitchPass();
-//   outlinePass = new OutlinePass(
-//     new THREE.Vector2(window.innerWidth, window.innerHeight),
-//     scene,
-//     camera
-//   );
-//   composer.addPass(outlinePass);
-//   const pass = new SMAAPass(
-//     window.innerWidth * renderer.getPixelRatio(),
-//     window.innerHeight * renderer.getPixelRatio()
-//   );
-//   composer.addPass(pass);
+ 
   /* ----------------------------- camera controls ---------------------------- */
 //   CameraControls.install({ THREE: THREE });
   clock = new THREE.Clock();
