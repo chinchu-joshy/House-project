b22310e0ee3465e7b8d7ba139d1fd7c22b61b87c
Chinchu Joshy <chinchu.joshy@ftsindia.in>
2022-06-23 11:24:02 +0530

    changed path30

--- dist/hittest.js
+++ dist/hittest.js
@@ -148,7 +148,7 @@ function init() {
     });
     fbxLoader.load("Model/vent.fbx", (object) => {
      console.log("checking")
-      object.position.set(0,0,0);
+      object.position.set(13, 43.8, 28.5);
       // object.position.set(13, 49.8, 14);
       object.traverse((child) => {
         if (child.isMesh && child.name.includes("Vent")) {
